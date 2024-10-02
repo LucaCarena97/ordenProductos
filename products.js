8d62229bc481712caea0342b235142b048e4d79a
lucacarena97 <lucacarena98@gmail.com>
2024-10-02 17:00:23 -0300

    Updates

---
+++ products.js
@@ -0,0 +1,18 @@
+import productsList from "./products.json" with { type: "json" };
+function getProductsSortedByPrice() {
+    return [...productsList].sort((a, b) => a.price - b.price);
+}
+export function productsListComponent() {
+    const section = document.createElement("section");
+    section.style.border = "solid 1px #ccc";
+    section.style.padding = "10px";
+    const list = document.createElement("ul");
+    const sortedProducts = getProductsSortedByPrice();
+    sortedProducts.forEach((product) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${product.title} - $${product.price}`;
+        list.appendChild(listItem);
+    });
+    section.appendChild(list);
+    return section;
+}
