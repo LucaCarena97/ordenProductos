d9081895a98a560e7e8fba3491ac0dfcc4b333b4
lucacarena97 <lucacarena98@gmail.com>
2024-10-02 16:59:04 -0300

    footer.ts

---
+++ src/footer.ts
@@ -0,0 +1,7 @@
+export function footerComponent() {
+  const footer = document.createElement("footer");
+  footer.innerHTML = "<h1>Componente Footer</h1>";
+  footer.style.border = "solid 3px black";
+  footer.style.padding = "12px";
+  return footer;
+}
